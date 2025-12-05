b3f00a2edb0466e7183063285a5f9fc45ddba687
macsilvermac-creator <macsilver.mac@gmail.com>
2025-12-05 02:56:12 -0300

    Add files via upload

--- import { defineConfig, loadEnv } from 'v.ts
+++ import { defineConfig, loadEnv } from 'v.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  // @ts-ignore
+  const cwd = process.cwd();
+  const env = loadEnv(mode, cwd, '');
   return {
     plugins: [react()],
     define: {
