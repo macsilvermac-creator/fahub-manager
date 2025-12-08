388d18c8b589affcd0eda52f65f65ac887aff2ba
Marcelo Roberto da Silveira <macsilver.mac@gmail.com>
2025-12-07 22:52:05 -0300

    correção final

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,6 +9,11 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, cwd, '');
   return {
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     },
