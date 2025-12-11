3949ef02153f0729e8c2e77dc972c2500b343aaf
Marcelo Roberto da Silveira <macsilver.mac@gmail.com>
2025-12-11 08:57:18 -0300

    error

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -22,7 +23,7 @@ export default defineConfig(({ mode }) => {
       minify: 'esbuild',
       outDir: 'dist',
       sourcemap: false,
-      chunkSizeWarningLimit: 2000, 
+      chunkSizeWarningLimit: 1200, 
       rollupOptions: {
         output: {
           manualChunks: (id) => {
