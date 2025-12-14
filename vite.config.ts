ed39b1e27deac31e95ccc87a3c81b15bb3a956a5
Marcelo Roberto da Silveira <macsilver.mac@gmail.com>
2025-12-14 15:28:31 -0300

    atual 999 erro 9

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  // Carrega variáveis de ambiente
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
@@ -29,12 +27,6 @@ export default defineConfig(({ mode }) => {
           },
         },
       },
-    },
-    server: {
-      headers: {
-        'Cross-Origin-Opener-Policy': 'same-origin',
-        'Cross-Origin-Embedder-Policy': 'require-corp',
-      },
-    },
+    }
   };
 });
