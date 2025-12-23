d20c02bf020ac41ab415566820dbfda1944fe94b
macsilvermac-creator <macsilver.mac@gmail.com>
2025-12-23 17:32:12 -0300

    errr

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,6 @@
 // Manually define Vite types since vite/client is missing in this environment
 interface ImportMetaEnv {
   readonly API_KEY: string;
-  readonly MODE: string;
-  readonly PROD: boolean;
-  readonly BASE_URL: string;
 }
 
 interface ImportMeta {
