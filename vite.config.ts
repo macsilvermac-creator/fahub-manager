5f28ac5a13adc03b5434ecade0126e3ab3846743
macsilvermac-creator <macsilver.mac@gmail.com>
2025-12-24 02:50:39 -0300

    er

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   }
