2992f3e97f720fe78e2b79ab2ad5f1c30a949730
macsilvermac-creator <macsilver.mac@gmail.com>
2025-12-24 15:07:05 -0300

    er

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-      'services': path.resolve(__dirname, './services'),
-      'components': path.resolve(__dirname, './components'),
-      'pages': path.resolve(__dirname, './pages')
-    }
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
+        main: './index.html',
       },
     },
+  },
+  server: {
+    port: 3000,
+    host: true
   }
 });
