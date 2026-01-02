194d9293ed871fd87ae8c636a05b48d5bbed9f61
macsilvermac-creator <macsilver.mac@gmail.com>
2026-01-02 11:49:30 -0300

    at

--- src/modules/athletes/types.ts
+++ src/modules/athletes/types.ts
@@ -6,6 +6,13 @@ export interface Athlete {
   position: string;
   number: number;
   status: AthleteStatus;
+  
+  // Campos Opcionais (podem estar vazios)
   height?: string;
   weight?: string;
+  category?: string;
+  email?: string;
+  phone?: string;
+  photo_url?: string;
+  created_at?: string;
 }
